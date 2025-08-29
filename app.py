6e36af3287f1184bcc5b53b61371fd066eefe164
enric-alavedra <enric.alavedra@prenomics.com>
2025-08-29 11:22:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -35,8 +35,6 @@ def recomendar_canciones(song_title, n_recommendations=10):
     return recommended_songs
 
 st.title("Music Recommender App")
-
-#song_list = df["track_name"].unique()
 song_list = sorted(df["track_name"].dropna().astype(str).unique())
 song_choice = st.selectbox("Elige una canción:", song_list)
 
@@ -49,5 +47,6 @@ if st.button("Recomendar"):
     else:
         st.subheader(f"🔎 Recomendaciones para '{song_choice}':")
         st.dataframe(recs.reset_index(drop=True))
+
 
 
